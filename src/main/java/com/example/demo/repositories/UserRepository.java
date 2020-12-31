7dceabf8b92495847fa46712357040d6e7568749
baudouin <keunne.baudoin@yahoo.fr>
2020-12-31 04:44:47 +0100

    Spring Securtiy with JWT

---
+++ src/main/java/com/example/demo/repositories/UserRepository.java
@@ -0,0 +1,20 @@
+package com.example.demo.repositories;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.models.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID>{
+
+	Optional<User> findByUsername(String username);
+
+	Boolean existsByUsername(String username);
+
+	Boolean existsByEmail(String email);
+
+}
---
+++ src/main/java/com/example/demo/service/UserDetailService.java
@@ -0,0 +1,28 @@
+package com.example.demo.service;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.models.User;
+import com.example.demo.repositories.UserRepository;
+
+@Service
+public class UserDetailService implements UserDetailsService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Transactional
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByUsername(username)
+				.orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+		return UserDetailsImpl.build(user);
+	}
+
+}
